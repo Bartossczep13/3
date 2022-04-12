9f7a637145ce029f3d3232148b24013d37ff1f20
karofelek666 <100691829+Bartossczep13@users.noreply.github.com>
2022-04-12 11:15:06 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,42 @@
+import java.io.IOException;
+import java.util.Scanner;
+class Main {
+  private static Scanner x;
+  public static void main(String[] args) {
+    try {
+    Service1 d = new Service1();
+      
+    int k;
+      System.out.println("wybierz funkcje ");
+      System.out.println(" 1. Dodanie studenta \n 2. Wypisanie listy studnetow  \n  3. koniec");
+      Scanner to = new Scanner(System.in);
+      k = to.nextInt();
+
+      switch(k)
+        {
+          case 1:
+          System.out.println("Podaj imie i wiek studenta: ");
+          x = new Scanner (System.in);
+          String i = x.nextLine();
+          int w = x.nextInt();
+          d.addStudent(new Student(i,w));
+            break;
+            case 2:
+          var students = d.getStudents();
+          for(Student current : students) {
+          System.out.println(current.ToString());
+            case 3:
+            break;
+        }
+      
+
+      
+
+      }
+    } catch (IOException e) {
+
+      
+
+    }
+  }
+}
